e7915119cca2da89d52bbcf6ec55c96fca2af7a5
shin <shin@yasuhironoMacBook-Pro.local>
2019-10-29 19:39:49 +0900

    create mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,43 +1,51 @@
-export const people = [
+export let movies = [
     {
         id: 0,
-        name: "woohee",
-        age: 24,
-        gender: "female",
+        name: "Avengers: Endgame",
+        score: 95
+       
     },
     {
         id: 1,
-        name: "yeajin",
-        age: 23,
-        gender: "female",
+        name: "Joker",
+        score: 94
+
     },
     {
         id: 2,
-        name: "jiwon",
-        age: 23,
-        gender: "female",
+        name: "parasite",
+        score: 99     
     },
     {
         id: 3,
-        name: "kanna",
-        age: 20,
-        gender: "female",
-    },
-    {
-        id: 4,
-        name: "fumika",
-        age: 23,
-        gender: "female",
-    },
-    {
-        id: 5,
-        name: "satomi",
-        age: 30,
-        gender: "female",
+        name: "SpiderMan: Home Comming",
+        score: 79       
     },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filterPeople = people.filter(person => person.id === id);
-    return filterPeople[0];
+    const filtermovies = movies.filter(movie => movie.id === id);
+    return filtermovies[0];
+}
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: movies.length + 1,
+        name,
+        score
+    }
+    movies.push(newMovie);
+    return newMovie;
 }
